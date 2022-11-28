330ea2a27c69c8afd40d8051375d2cf3ea809856
Salinamalek <salinamalek@pursuit.org>
2022-11-28 13:25:07 -0500

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import Modal from "./Modal";
+import "./Modal.css";
+import { Link } from "react-router-dom";
+import Search from "./Search";
+
+export default function Header({ searchTitle, setSearchTitle }) {
+  return (
+    <div>
+      <header>
+        <h1>
+          <span className="you">You</span>
+          <span className="tube">Tube</span>
+          <span className="homeAndAboutLinks">
+            <Link to="/" className="linkColor">
+              Home
+            </Link>
+            <Link to="/about" className="linkColor">
+              About
+            </Link>
+            {/* <Modal /> */}
+          </span>
+          <span id="searchForm">
+            <Search setSearchTitle={setSearchTitle} searchTitle={searchTitle} />
+          </span>
+        </h1>
+      </header>
+    </div>
+  );
+}
