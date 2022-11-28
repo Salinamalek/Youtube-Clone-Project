f60d373fed85440d70535b3c7a58c170c70fb0b0
Salinamalek <salinamalek@pursuit.org>
2022-11-28 13:56:11 -0500

    project done

--- src/components/DevsAbout.js
+++ src/components/DevsAbout.js
@@ -5,8 +5,12 @@ export default function DevsAbout() {
         <h1>Salina Malek</h1>
         <p>
           Hello world! My name is Salina (pronounced as Selena or 'suh-lee-na')
-          and I am a fellow at Pursuit studying to become a fullstack web
-          developer!
+          and I am a student at Pursuit studying to become a fullstack web
+          developer that positively impacts the world! As someone who enjoys
+          volunteering and helping others, I hope to use the skills and
+          knowledge obtained through Pursuit to develop applications that assist
+          people with the means to improve their health, lifestyle, and quality
+          of life with my shift into tech.
         </p>
         <a
           href="https://github.com/Salinamalek"
@@ -18,7 +22,14 @@ export default function DevsAbout() {
       </div>
       <div className="daniel">
         <h1>Daniel Rodriguez</h1>
-        <p>Hello World</p>
+        <p>
+          Hello, my name is Daniel Rodriguez and I am a Full Stack Web
+          Development Fellow at the Pursuit Fellowship based out of New York
+          City. The Pursuit Fellowship is an intensive 12 month software
+          engineering fellowship with a 9% acceptance rate and a project-based
+          curriculum which provides experience with languages such as
+          Javascript, CSS, and HTML.
+        </p>
         <a
           href="https://github.com/DanielR2105"
           target="_blank"
